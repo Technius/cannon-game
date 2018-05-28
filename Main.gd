526b4b7961815c9a2111072670a01ee4a8ab763d
Bryan Tan <techniux@gmail.com>
2018-05-27 22:22:47 -0700

    Remove projectiles when they leave the level

--- Main.gd
+++ Main.gd
@@ -4,6 +4,7 @@ var show_window = true
 func _ready():
 	$Ball.connect("body_entered", self, "ball_collide")
 	$GUI/ResetButton.connect("button_down", self, "reset_level")
+	$LevelBounds.connect("body_exited", self, "on_object_oob")
 
 func ball_collide(other):
 	if other.is_in_group("goal"):
@@ -26,3 +27,9 @@ func _input(event):
 func reset_level():
 	get_tree().reload_current_scene()
 
+# When an object leaves the level bounds
+func on_object_oob(object):
+	if object.is_in_group("projectile"):
+		self.remove_child(object)
+	elif object == $Ball:
+		print("You lose")
